ed4ca83e604b059aa65d5a3dc926caa13ccfe239
Mark Alston <malston@pivotal.io>
2015-06-25 15:46:20 -0700

    Added concourse ci config

---
+++ ci/scripts/create-release/run.sh
@@ -0,0 +1,14 @@
+echo $PWD
+
+export RELEASE_NAME=${RELEASE_NAME:-ardo}
+export RELEASE_VERSION=${RELEASE_VERSION:-1+dev.1}
+export RELEASE_FILE=$RELEASE_NAME-$RELEASE_VERSION.tgz
+
+
+ls repo/
+
+echo Creating a bosh release
+cd repo
+bosh -n create release --name $RELEASE_NAME --version $RELEASE_VERSION --force
+
+ls -al dev_releases/
